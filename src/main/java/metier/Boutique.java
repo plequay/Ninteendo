12b5806985d9f6e1a48b5452a15ae1615e90cd28
Nicolas Kroupa <nicolas.kroupa@gmail.com>
2021-07-26 15:31:59 +0200

    add Boutique class ++

--- src/main/java/metier/Boutique.java
+++ src/main/java/metier/Boutique.java
@@ -7,6 +7,45 @@ public class Boutique {
 	protected String nom;
 	protected String Adresse;
 	protected List<String> Jeux;
+	
+	
+	public Boutique(String nom, String adresse, List<String> jeux) {
+		super();
+		this.nom = nom;
+		Adresse = adresse;
+		Jeux = jeux;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getAdresse() {
+		return Adresse;
+	}
+	
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	}
+	
+	public List<String> getJeux() {
+		return Jeux;
+	}
+	
+	public void setJeux(List<String> jeux) {
+		Jeux = jeux;
+	}
+
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", Adresse=" + Adresse + ", Jeux=" + Jeux + "]";
+	}
 		
+	
+	
 	
 }
