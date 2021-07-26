f49b1e9bc9ce9004100dc82def781dfaa9b1acdd
Johan <johan.loison@yahoo.com>
2021-07-26 16:03:41 +0200

    modification console

--- src/main/java/metier/Console.java
+++ src/main/java/metier/Console.java
@@ -1,12 +1,20 @@
 package metier;
 
+import java.time.LocalDate;
+
 public class Console {
 
 	private String nom;
+	private LocalDate dateSortie;
+	private double prix;
 
-	public Console(String nom) {
+	public Console(String nom, LocalDate dateSortie, double prix) {
+		super();
 		this.nom = nom;
+		this.dateSortie = dateSortie;
+		this.prix = prix;
 	}
+
 
 	public String getNom() {
 		return nom;
@@ -16,10 +24,33 @@ public class Console {
 		this.nom = nom;
 	}
 
+	public LocalDate getDateSortie() {
+		return dateSortie;
+	}
+
+
+	public void setDateSortie(LocalDate dateSortie) {
+		this.dateSortie = dateSortie;
+	}
+
+
+	public double getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", dateSortie=" + dateSortie + ", prix=" + prix + "]";
 	}
+
+
+	
 	
 	
 }
